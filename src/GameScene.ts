f46939a1b4ce566f107932b65c08373a74640eda
L. K <LK00100100@gmail.com>
2023-09-06 12:30:16 -0400

    man and tile collision. cannot input when moving

--- src/GameScene.ts
+++ src/GameScene.ts
@@ -8,24 +8,44 @@ export default class GameScene extends Phaser.Scene {
 
   private gameText!: Phaser.GameObjects.Text; //any messages for the player to read.
 
+  private map: Phaser.Tilemaps.Tilemap;
+
+  private man: Phaser.Types.Physics.Arcade.ImageWithDynamicBody;
+
+  private layer: Phaser.Tilemaps.TilemapLayer;
+
+  private readonly PLAYER_SPEED_DEFAULT = 500;
+
   constructor() {
     super("GameScene");
   }
 
   preload() {
+    //level
     this.load.tilemapCSV("map", "assets/tiles.csv");
     this.load.image("tiles", "assets/tiles.png");
+
+    //people
+    this.load.image("man", "assets/man.png");
   }
 
   create() {
-    const map = this.make.tilemap({
+    /**
+     * make level
+     */
+    this.map = this.make.tilemap({
       key: "map",
       tileWidth: 32,
       tileHeight: 32,
     });
-    const tileset = map.addTilesetImage("tiles");
-    const layer = map.createLayer(0, tileset, 0, 0); // layer index, tileset, x, y
-    layer.skipCull = true;
+    const tileset = this.map.addTilesetImage("tiles");
+    this.layer = this.map.createLayer(0, tileset, 0, 0); // layer index, tileset, x, y
+    this.layer.skipCull = true;
+
+    // Or, you can set collision on all indexes within an array
+    this.map.setCollisionBetween(0, 15);
+
+    //this.showCollision();
 
     this.gameText = this.add.text(10, 10, "test text");
 
@@ -36,24 +56,49 @@ export default class GameScene extends Phaser.Scene {
     this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
     this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
     this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+
+    /**
+     * person
+     */
+    this.man = this.physics.add.sprite(48, 272, "man");
+
+    /**
+     * camera
+     */
+    this.cameras.main.setBounds(0, 0, 400, 400);
+    this.cameras.main.setZoom(1.5);
+    this.cameras.main.centerOn(0, 0);
   }
 
   update() {
-    if (Phaser.Input.Keyboard.JustDown(this.keyW)) {
-      this.setGameText("w");
+    if (!this.checkIsManMoving()) {
+      if (Phaser.Input.Keyboard.JustDown(this.keyW)) {
+        this.setGameText("w");
+        this.man.setVelocityX(0);
+        this.man.setVelocityY(-this.PLAYER_SPEED_DEFAULT);
+      }
+
+      if (Phaser.Input.Keyboard.JustDown(this.keyA)) {
+        this.setGameText("a");
+        this.man.setVelocityX(-this.PLAYER_SPEED_DEFAULT);
+        this.man.setVelocityY(0);
+      }
+
+      if (Phaser.Input.Keyboard.JustDown(this.keyS)) {
+        this.setGameText("s");
+        this.man.setVelocityX(0);
+        this.man.setVelocityY(this.PLAYER_SPEED_DEFAULT);
+      }
+
+      if (Phaser.Input.Keyboard.JustDown(this.keyD)) {
+        this.setGameText("d");
+        this.man.setVelocityX(this.PLAYER_SPEED_DEFAULT);
+        this.man.setVelocityY(0);
+      }
     }
 
-    if (Phaser.Input.Keyboard.JustDown(this.keyA)) {
-      this.setGameText("A");
-    }
-
-    if (Phaser.Input.Keyboard.JustDown(this.keyS)) {
-      this.setGameText("S");
-    }
-
-    if (Phaser.Input.Keyboard.JustDown(this.keyD)) {
-      this.setGameText("D");
-    }
+    //  Collide player against the tilemap layer
+    this.physics.collide(this.man, this.layer);
   }
 
   /**
@@ -61,5 +106,17 @@ export default class GameScene extends Phaser.Scene {
    */
   setGameText(newText: string) {
     this.gameText.text = newText;
+  }
+
+  /**
+   * Visualize the colliding tiles
+   */
+  private showCollision() {
+    const debugGraphics = this.add.graphics();
+    this.map.renderDebug(debugGraphics);
+  }
+
+  private checkIsManMoving(): boolean {
+    return this.man.body.velocity.x != 0 || this.man.body.velocity.y != 0;
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import GameScene from "./GameScene";
-import * as Phaser from 'phaser';
+import * as Phaser from "phaser";
 
-const config = {
-    type: Phaser.AUTO,
-    backgroundColor: '#125555',
-    width: 800,
-    height: 600,
-    scene: GameScene
+var config = {
+  type: Phaser.AUTO,
+  backgroundColor: "#125555",
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+    },
+  },
+  width: 800,
+  height: 600,
+  scene: GameScene,
 };
 
 const game = new Phaser.Game(config);
